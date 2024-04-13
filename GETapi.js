57e78adfad9ea5bc80d7fb6b10fd07d758d09139
shreyanshgupta123 <145015168+shreyanshgupta123@users.noreply.github.com>
2024-04-14 02:16:46 +0530

    14/04/2024

--- GETapi.js
+++ GETapi.js
@@ -1,17 +1,33 @@
-const express=require('express');
+const express = require('express');
 const app = express();
-const port= process.env.PORT|| 3000
-const data=require('./Service.json')
-const cors=require('cors')
-app.get('/',(req,res)=>{
-    res.send('Hello World');
-}); 
+const port = process.env.PORT || 3000;
+const getmenu = require('./Getmenu.json');
+const getallrestaurant = require('./Get_all_restaurants.json');
+const getallfood = require('./Getmenu.json'); // Assuming this should be require('./Get_all_food_items.json')
+const cors = require('cors');
+
 app.use(cors());
 
-app.get('/get',(req,res)=>{
-    res.send(data);
-});
-app.listen(port,()=>
-{
+app.route('/')
+    .get((req, res) => {
+        res.send('Prod api');
+    });
+
+app.route('/food_get_menu')
+    .get((req, res) => {
+        res.send(getmenu);
+    });
+
+app.route('/food_get_all_restaurant')
+    .get((req, res) => {
+        res.send(getallrestaurant);
+    });
+
+app.route('/food_get_all_food_items')
+    .get((req, res) => {
+        res.send(getallfood); // Assuming this should be getallfood
+    });
+
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
